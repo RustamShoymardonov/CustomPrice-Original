c1459881e6359f7e8b150695a227bb70ec59c49a
Bahodir <imomuddinov@inbox.ru>
2021-12-15 12:54:12 +0500

    Initial commit

---
+++ src/main/java/uz/customs/customsprice/CustomspriceApplication.java
@@ -0,0 +1,20 @@
+package uz.customs.customsprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class CustomspriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomspriceApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CustomspriceApplication.class);
+    }
+
+}
---
+++ src/test/java/uz/customs/customsprice/CustomspriceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.customs.customsprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomspriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
