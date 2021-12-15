4371766aa34d47ba4b84da756737785e25a022f1
ADMIN <rustam_bek.1989r@mail.ru>
2021-12-15 17:05:02 +0500

    Rustamhoney

--- out/artifacts/CUSTOMSPRICE/index.jsp
+++ out/artifacts/CUSTOMSPRICE/index.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: ADMIN
   Date: 15.12.2021
-  Time: 11:52
+  Time: 16:51
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-<h1>SALOM</h1>
+<h1>Rustamhoney</h1>
 </body>
 </html>
--- src/main/java/uz/customs/customsprice/controllers/MainControllerCustomsPrice.java
+++ src/main/java/uz/customs/customsprice/controllers/MainControllerCustomsPrice.java
@@ -1,18 +1,20 @@
 package uz.customs.customsprice.controllers;
 
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainControllerCustomsPrice {
 
-    @GetMapping("/")
-    public String greeting(Model model) {
-        model.addAttribute("name", "name");
+//    @GetMapping("/")
+//    public ModelAndView index() {
+//        ModelAndView model = new ModelAndView("index");
+//        return model;
+//    }
+
+    @GetMapping(value = "/")
+    public String home() {
+
         return "index";
     }
-
 }
